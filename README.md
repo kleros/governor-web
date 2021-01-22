981092347cee6f5883e110471ed079ce7192da52
epiqueras <epiquerass@gmail.com>
2021-01-22 10:38:26 -0800

    chore: build subgraph schema

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## (2021-01-22)
 
+- chore: generate first changelog ([2e41de7](https://github.com/kleros/governor-web/commit/2e41de7))
 - chore: set up project and architecture ([d28fb74](https://github.com/kleros/governor-web/commit/d28fb74))
--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,9 +1,44 @@
-import { BigInt } from "@graphprotocol/graph-ts";
-import { ListSubmitted } from "../generated/Governor/Governor";
-import { Contract } from "../generated/schema";
+import { ByteArray } from "@graphprotocol/graph-ts";
 
-export function listSubmitted(event: ListSubmitted): void {
-  let ID = new BigInt(0);
-  let contract = new Contract(ID.toString());
+import { Governor, SetMetaEvidenceCall } from "../generated/Governor/Governor";
+import { Contract, MetaEvidence } from "../generated/schema";
+
+function getStatus(status: number): string {
+  if (status == 0) return "NoDispute";
+  if (status == 1) return "DisputeCreated";
+  if (status == 2) return "Resolved";
+  return "Error";
+}
+
+function concatByteArrays(a: ByteArray, b: ByteArray): ByteArray {
+  let out = new Uint8Array(a.length + b.length);
+  for (let i = 0; i < a.length; i++) out[i] = a[i];
+  for (let j = 0; j < b.length; j++) out[a.length + j] = b[j];
+  return out as ByteArray;
+}
+
+export function setMetaEvidence(call: SetMetaEvidenceCall): void {
+  let governor = Governor.bind(call.to);
+
+  let contract = new Contract("0");
+  contract.deployer = call.from;
+  contract.reservedETH = governor.reservedETH();
+  contract.submissionBaseDeposit = governor.submissionBaseDeposit();
+  contract.submissionTimeout = governor.submissionTimeout();
+  contract.executionTimeout = governor.executionTimeout();
+  contract.withdrawTimeout = governor.withdrawTimeout();
+  contract.sharedMultiplier = governor.sharedMultiplier();
+  contract.winnerMultiplier = governor.winnerMultiplier();
+  contract.loserMultiplier = governor.loserMultiplier();
+  contract.lastApprovalTime = governor.lastApprovalTime();
+  contract.metaEvidenceUpdates = governor.metaEvidenceUpdates();
+
+  let metaEvidence = new MetaEvidence(
+    contract.metaEvidenceUpdates.toHexString()
+  );
+  metaEvidence.URI = call.inputs._metaEvidence;
+  metaEvidence.save();
+
+  contract.metaEvidence = metaEvidence.id;
   contract.save();
 }
